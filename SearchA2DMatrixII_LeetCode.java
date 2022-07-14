89ea55e45c837921278b32f49ee75ecab273221a
Uttkarsh <uttkarshpatel9@gmail.com>
2022-07-14 22:40:48 +0530

    Added faster solution for Day 3

--- SearchA2DMatrixII_LeetCode.java
+++ SearchA2DMatrixII_LeetCode.java
@@ -2,11 +2,40 @@
 
 public class SearchA2DMatrixII_LeetCode {
     public static void main(String[] args) {
-        int[][] matrix = {{1,4,7,11,15},{2,5,8,12,19},{3,6,9,16,22},{10,13,14,17,24},{18,21,23,26,30}};
+        int[][] matrix = {
+            {1,4,7,11,15},
+            {2,5,8,12,19},
+            {3,6,9,16,22},
+            {10,13,14,17,24},
+            {18,21,23,26,30}};
         int target = 22;
 
         Solution s = new Solution();
+        EfficientSolution es = new EfficientSolution();
+        
+        System.out.println(es.searchMatrix(matrix, target));
         System.out.println(s.searchMatrix(matrix, target));
+    }
+}
+
+class EfficientSolution {
+    public boolean searchMatrix(int[][] matrix, int target){
+        int column = matrix[0].length-1, row = 0;
+        int element;
+
+        while((column >= 0) && (row <= matrix.length-1)){
+            element = matrix[row][column];
+
+            if(element == target){
+                return true;
+            }
+            else if (element < target){
+                ++row;
+            } else {
+                --column;
+            }
+        }
+        return false;
     }
 }
 
