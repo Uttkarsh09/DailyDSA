c47c35e915b156b708c6de6dfa7a52cefa937cc0
Uttkarsh <uttkarshpatel9@gmail.com>
2022-07-18 13:57:21 +0530

    Day 7

---
+++ PowerOfTwo_LeetCode.java
@@ -0,0 +1,33 @@
+// https://leetcode.com/problems/power-of-two/
+
+public class PowerOfTwo_LeetCode {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int n = 1024;
+        boolean ans = s.isPowerOfTwo(n);
+        System.out.println(ans);
+    }
+}
+
+// Let's take some good exampes :
+// number->binary form
+// 3->0000 0011
+// 4->0000 0100
+// 5->0000 0101
+// 6->0000 0110
+// 7->0000 0111
+// 8->0000 1000
+// The observation we can conclude is that the number which is a power of two has always compulsoryily 1 true bit.
+// Now consider :-
+// bit representation of 7  -> 0111
+// bit representation of 8  -> 1000
+// bitwise AND of 7 and 8 -> 0111 & 1000 => 0000
+// we are gonna use this property for for all numbers which are powers of two
+// Reference - https://leetcode.com/problems/power-of-two/discuss/1638704/C%2B%2B-EASY-TO-SOLVE-oror-All-INTERVIEW-APPROACHES-with-Detailed-Explanations
+
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if(n<=0) return false;
+        return (n & (n-1)) == 0;
+    }
+}
